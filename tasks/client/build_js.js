284ee3d39e77e49093fda832a83efe89913eb8b5
ughuuu <daian.dragos@yahoo.com>
2017-01-15 15:56:41 +0200

    test es5

--- tasks/client/build_js.js
+++ tasks/client/build_js.js
@@ -1,12 +1,15 @@
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import {path, tasks} from './const';
+var babel  = require('gulp-babel');
+var plumber = require('gulp-plumber');
 
 const JS = path.DIST + '**/*.js';
 
 gulp.task(tasks.CLIENT_JS_DIST, () => {
-  return gulp.src(JS, {base: path.DIST})
+  return gulp.src(JS, {base: path.DIST})    
+             .pipe(plumber())
+             .pipe(babel({presets: ['es2015']}))
              .pipe(uglify())
              .pipe(gulp.dest(path.DIST));
-})
-.on('error', console.log);
+});
--- tasks/client/index.js
+++ tasks/client/index.js
@@ -4,8 +4,7 @@ import {tasks} from './const';
 
 gulp.task(tasks.CLIENT_BUILD_DEV, [
   tasks.CLIENT_BUILD_TS
-])
-.on('error', console.log);
+]);
 
 gulp.task(tasks.CLIENT_BUILD_DIST, () => {
   return new Promise((resolve, reject) => {
@@ -17,11 +16,10 @@ gulp.task(tasks.CLIENT_BUILD_DIST, () => {
               tasks.CLIENT_VIEWS_DIST,
               [
                 tasks.CLIENT_IMAGE_DIST,
-                //tasks.CLIENT_JS_DIST
+                tasks.CLIENT_JS_DIST
               ],
               
               resolve
             );  
   });  
-})
-.on('error', console.log);
+});
